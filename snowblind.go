af9705699b1f9947b04a886f55d10a26bb38cc25
Franklin Harding <fharding1@protonmail.com>
2024-08-31 13:06:21 -0400

    Godoc and check verify in NewSignature

--- snowblind.go
+++ snowblind.go
@@ -26,15 +26,21 @@ var generatorH = sync.OnceValue(func() *r255.Element {
 	return r255.NewElement().FromUniformBytes(digest)
 })
 
+// PublicKey is used for obtaining and verifying Snowblind signatures.
 type PublicKey struct {
 	point *r255.Element
 }
 
+var _ crypto.PublicKey = &PublicKey{}
+
+// NewPublicKey returns the Snowblind public key corresponding to the 32-byte key
+// given as an argument.
 func NewPublicKey(key []byte) (*PublicKey, error) {
 	point, err := r255.NewIdentityElement().SetCanonicalBytes(key)
 	return &PublicKey{point: point}, err
 }
 
+// Bytes returns the 32-byte representation of pk
 func (pk *PublicKey) Bytes() []byte {
 	if pk.point != nil {
 		return pk.point.Bytes()
@@ -43,6 +49,7 @@ func (pk *PublicKey) Bytes() []byte {
 	return nil
 }
 
+// Equal returns true if op is a Snowblind public key and is equal to pk.
 func (pk *PublicKey) Equal(op crypto.PublicKey) bool {
 	otherPK, ok := op.(*PublicKey)
 	if !ok {
@@ -52,21 +59,30 @@ func (pk *PublicKey) Equal(op crypto.PublicKey) bool {
 	return otherPK.point.Equal(pk.point) == 1
 }
 
+// PrivateKey is used for signing Snowblind signatures.
 type PrivateKey struct {
 	multiplier *r255.Scalar
 }
 
+var _ crypto.PrivateKey = &PrivateKey{}
+
+// NewPrivateKey returns the Snowblind private key corresponding to the
+// given 32-byte key.
 func NewPrivateKey(key []byte) (*PrivateKey, error) {
 	multiplier, err := r255.NewScalar().SetCanonicalBytes(key)
 	return &PrivateKey{multiplier: multiplier}, err
 }
 
+// Public returns the public key corresponding to sk. To meet the crypto library
+// PrivateKey interface, the return type is crypto.PublicKey, so users will need
+// to type assert to PublicKey.
 func (sk *PrivateKey) Public() crypto.PublicKey {
 	pk := &PublicKey{}
 	pk.point = r255.NewIdentityElement().ScalarBaseMult(sk.multiplier)
 	return pk
 }
 
+// Bytes returns the 32-byte representation of sk.
 func (sk *PrivateKey) Bytes() []byte {
 	if sk.multiplier != nil {
 		return sk.multiplier.Bytes()
@@ -75,6 +91,7 @@ func (sk *PrivateKey) Bytes() []byte {
 	return nil
 }
 
+// Equal returns true if op is a Snowblind PrivateKey and is equal to sk
 func (sk *PrivateKey) Equal(op crypto.PrivateKey) bool {
 	otherSK, ok := op.(*PrivateKey)
 	if !ok {
@@ -84,6 +101,8 @@ func (sk *PrivateKey) Equal(op crypto.PrivateKey) bool {
 	return otherSK.multiplier.Equal(sk.multiplier) == 1
 }
 
+// GenerateKey returns a PrivateKey using bytes read from rand. Users should basically
+// always use crypto.Rand as an argument here.
 func GenerateKey(rand io.Reader) (*PrivateKey, error) {
 	randomBuffer := make([]byte, 64)
 
@@ -100,6 +119,8 @@ func GenerateKey(rand io.Reader) (*PrivateKey, error) {
 	return &PrivateKey{multiplier: multiplier}, nil
 }
 
+// SignerState keeps track of information needed to sign a Snowblind signature and can only
+// be used once per signature.
 type SignerState struct {
 	sk       *r255.Scalar
 	a, b     *r255.Scalar // elements of Zp
@@ -107,10 +128,13 @@ type SignerState struct {
 	finished bool
 }
 
+// NewSignerState returns a signer state which can be used to sign a Snowblind signature.
 func (sk *PrivateKey) NewSignerState() *SignerState {
 	return &SignerState{sk: sk.multiplier}
 }
 
+// NewCommitment returns a commitment which should be sent to the user as the first move of
+// communication in the three move protocol.
 func (ss *SignerState) NewCommitment() ([]byte, error) {
 	if ss.a != nil {
 		return nil, errors.New("this signer state was already used to generate a commitment")
@@ -148,11 +172,14 @@ func (ss *SignerState) NewCommitment() ([]byte, error) {
 	return append(A.Bytes(), B.Bytes()...), nil
 }
 
+// UserState keeps track of information needed to obtain a signature from a signer, and can only
+// be used once per signature.
 type UserState struct {
-	pk       *r255.Element
+	pk       *PublicKey
 	cmtA     *r255.Element
 	cmtB     *r255.Element
 	bigRBar  *r255.Element
+	msg      []byte
 	c        *r255.Scalar
 	r        *r255.Scalar
 	alpha    *r255.Scalar
@@ -160,13 +187,14 @@ type UserState struct {
 	finished bool
 }
 
+// NewUserState returns a new user state which can be used to obtain a signature from a signer.
 func (pk *PublicKey) NewUserState() *UserState {
-	return &UserState{pk: pk.point}
+	return &UserState{pk: pk}
 }
 
+// scalarIsZero returns true if the given scalar is equal to zero
 func scalarIsZero(s *r255.Scalar) bool {
-	zero := r255.NewScalar().Zero()
-	return s.Equal(zero) == 1
+	return s.Equal(r255.NewScalar().Zero()) == 1
 }
 
 // simple square-and-multiply for s^5
@@ -177,7 +205,10 @@ func scalarPowFive(s *r255.Scalar) *r255.Scalar {
 	return res
 }
 
-func (us *UserState) NewChallenge(cmt []byte, hash []byte) ([]byte, error) {
+// NewChallenge takes a commitment (cmt) from the signer and a message (msg) to be signed,
+// and returns a challenge which should be sent back to the signer as the second move of communication
+// in the three move protocol.
+func (us *UserState) NewChallenge(cmt []byte, msg []byte) ([]byte, error) {
 	if us.cmtA != nil {
 		return nil, errors.New("user state has already been used to generate a challenge")
 	}
@@ -221,12 +252,12 @@ func (us *UserState) NewChallenge(cmt []byte, hash []byte) ([]byte, error) {
 
 	alphaPowFive := scalarPowFive(alpha)
 
-	bigRBar := r255.NewIdentityElement().ScalarBaseMult(r)                                                           // bigRBar = rG
-	bigRBar.Add(bigRBar, r255.NewIdentityElement().ScalarMult(alphaPowFive, us.cmtA))                                // bigRBar = rG + (alpha^5) A
-	bigRBar.Add(bigRBar, r255.NewIdentityElement().ScalarMult(r255.NewScalar().Multiply(alphaPowFive, beta), us.pk)) // bigRBar = rG + (alpha^5) A + (alpha^5 beta) pk
-	bigRBar.Add(bigRBar, r255.NewIdentityElement().ScalarMult(alpha, us.cmtB))                                       // bigRBar = rG + (alpha^5) A + (alpha^5 beta) pk + alpha B
+	bigRBar := r255.NewIdentityElement().ScalarBaseMult(r)                                                                 // bigRBar = rG
+	bigRBar.Add(bigRBar, r255.NewIdentityElement().ScalarMult(alphaPowFive, us.cmtA))                                      // bigRBar = rG + (alpha^5) A
+	bigRBar.Add(bigRBar, r255.NewIdentityElement().ScalarMult(r255.NewScalar().Multiply(alphaPowFive, beta), us.pk.point)) // bigRBar = rG + (alpha^5) A + (alpha^5 beta) pk
+	bigRBar.Add(bigRBar, r255.NewIdentityElement().ScalarMult(alpha, us.cmtB))                                             // bigRBar = rG + (alpha^5) A + (alpha^5 beta) pk + alpha B
 
-	cBar, err := sigHash(us.pk, hash, bigRBar)
+	cBar, err := sigHash(us.pk.point, msg, bigRBar)
 	if err != nil {
 		return nil, err
 	}
@@ -235,6 +266,7 @@ func (us *UserState) NewChallenge(cmt []byte, hash []byte) ([]byte, error) {
 	c.Add(c, beta)                                                              // c = cBar * (alpha^5)^(-1) + beta
 
 	us.c = c
+	us.msg = msg
 	us.bigRBar = bigRBar
 	us.r = r
 	us.alpha = alpha
@@ -270,6 +302,8 @@ func (rsp *response) SetCanonicalBytes(b []byte) error {
 	return err
 }
 
+// NewResponse takes a challenge (chalBytes) and returns a response which should be sent to the user
+// to allow them to obtain the final signature on their message.
 func (ss *SignerState) NewResponse(chalBytes []byte) ([]byte, error) {
 	if ss.a == nil {
 		return nil, errors.New("this signer state has not been user to generate a commitment yet")
@@ -321,7 +355,8 @@ func (s *signature) SetCanonicalBytes(b []byte) error {
 	return err
 }
 
-func (us *UserState) NewSignature(rspBytes []byte) ([]byte, error) {
+// NewSignature takes a response (rsp) from the signer and returns the completed signature.
+func (us *UserState) NewSignature(rsp []byte) ([]byte, error) {
 	if us.cmtA == nil {
 		return nil, errors.New("this user state has not yet been used to generate a challenge")
 	}
@@ -330,56 +365,62 @@ func (us *UserState) NewSignature(rspBytes []byte) ([]byte, error) {
 		return nil, errors.New("this user state has already been used to generate a signature")
 	}
 
-	var rsp response
-	if err := rsp.SetCanonicalBytes(rspBytes); err != nil {
+	var decodedRsp response
+	if err := decodedRsp.SetCanonicalBytes(rsp); err != nil {
 		return nil, err
 	}
 
-	checkB := r255.NewIdentityElement().ScalarBaseMult(rsp.b)                     // checkB = bG
-	checkB.Add(checkB, r255.NewIdentityElement().ScalarMult(rsp.y, generatorH())) // checkB = bG + yH
+	checkB := r255.NewIdentityElement().ScalarBaseMult(decodedRsp.b)                     // checkB = bG
+	checkB.Add(checkB, r255.NewIdentityElement().ScalarMult(decodedRsp.y, generatorH())) // checkB = bG + yH
 
 	if checkB.Equal(us.cmtB) == 0 {
 		return nil, errors.New("signer did not honestly generate the response: checkB != cmtB")
 	}
 
-	zG := r255.NewIdentityElement().ScalarBaseMult(rsp.z)
+	zG := r255.NewIdentityElement().ScalarBaseMult(decodedRsp.z)
 
-	yPowFive := scalarPowFive(rsp.y)
+	yPowFive := scalarPowFive(decodedRsp.y)
 
-	checkzG := r255.NewIdentityElement().Add(us.cmtA, r255.NewIdentityElement().ScalarMult(r255.NewScalar().Add(us.c, yPowFive), us.pk)) // checkzG = A + (c + y^5) pk
+	checkzG := r255.NewIdentityElement().Add(us.cmtA, r255.NewIdentityElement().ScalarMult(r255.NewScalar().Add(us.c, yPowFive), us.pk.point)) // checkzG = A + (c + y^5) pk
 
 	if checkzG.Equal(zG) == 0 {
 		return nil, errors.New("signer did not honestly generate the response: checkzG != zG")
 	}
 
-	zBar := r255.NewScalar().Add(us.r, r255.NewScalar().Multiply(scalarPowFive(us.alpha), rsp.z)) // zBar = r + alpha^5 z
-	zBar.Add(zBar, r255.NewScalar().Multiply(us.alpha, rsp.b))                                    // zBar = r + alpha^5 z + alpha b
+	zBar := r255.NewScalar().Add(us.r, r255.NewScalar().Multiply(scalarPowFive(us.alpha), decodedRsp.z)) // zBar = r + alpha^5 z
+	zBar.Add(zBar, r255.NewScalar().Multiply(us.alpha, decodedRsp.b))                                    // zBar = r + alpha^5 z + alpha b
 
-	yBar := r255.NewScalar().Multiply(us.alpha, rsp.y)
+	yBar := r255.NewScalar().Multiply(us.alpha, decodedRsp.y)
 
 	us.finished = true
 
 	sig := &signature{bigRBar: us.bigRBar, zBar: zBar, yBar: yBar}
-	// TODO: add check that signature verifies
+
+	// I'm a little uncertain as to why this is necessary, but it's in the paper.
+	if !Verify(us.pk, sig.Bytes(), us.msg) {
+		return nil, errors.New("signature does not verify")
+	}
+
 	return sig.Bytes(), nil
 }
 
-func Verify(pk *PublicKey, sigBytes []byte, message []byte) bool {
-	sig := &signature{}
-	if err := sig.SetCanonicalBytes(sigBytes); err != nil {
+// Verify checks that a signature (sig) is valid relative to the given message (msg) and public key (pk).
+func Verify(pk *PublicKey, sig []byte, msg []byte) bool {
+	decodedSig := &signature{}
+	if err := decodedSig.SetCanonicalBytes(sig); err != nil {
 		return false
 	}
 
-	cBar, err := sigHash(pk.point, message, sig.bigRBar)
-	if err != nil || sig.yBar.Equal(r255.NewScalar().Zero()) == 1 {
+	cBar, err := sigHash(pk.point, msg, decodedSig.bigRBar)
+	if err != nil || scalarIsZero(decodedSig.yBar) {
 		return false
 	}
 
-	f := r255.NewScalar().Add(cBar, scalarPowFive(sig.yBar)) // f = cBar + yBar ^5
+	f := r255.NewScalar().Add(cBar, scalarPowFive(decodedSig.yBar)) // f = cBar + yBar ^5
 
-	leftCheck := r255.NewIdentityElement().Add(sig.bigRBar, r255.NewIdentityElement().ScalarMult(f, pk.point)) // leftCheck = Rbar + (cBar + yBar ^5) pk
-	rightCheck := r255.NewIdentityElement().Add(r255.NewIdentityElement().ScalarBaseMult(sig.zBar),
-		r255.NewIdentityElement().ScalarMult(sig.yBar, generatorH())) // rightCheck = zBar G + yBar H
+	leftCheck := r255.NewIdentityElement().Add(decodedSig.bigRBar, r255.NewIdentityElement().ScalarMult(f, pk.point)) // leftCheck = Rbar + (cBar + yBar ^5) pk
+	rightCheck := r255.NewIdentityElement().Add(r255.NewIdentityElement().ScalarBaseMult(decodedSig.zBar),
+		r255.NewIdentityElement().ScalarMult(decodedSig.yBar, generatorH())) // rightCheck = zBar G + yBar H
 
 	return leftCheck.Equal(rightCheck) == 1
 }
